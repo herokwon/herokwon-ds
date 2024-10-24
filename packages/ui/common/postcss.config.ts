53ee653fb468eb3bca2498ace57d441f8d945159
herokwon <contact@herokwon.dev>
2024-10-25 00:14:57 +0900

    Chore (ds-common): 패키지 생성 및 초기 환경 설정

---
+++ packages/ui/common/postcss.config.ts
@@ -0,0 +1,9 @@
+import type { Config } from 'postcss-load-config';
+
+import postcssConfig from '../../tailwind-config/postcss.config';
+
+const config: Config = {
+  ...postcssConfig,
+};
+
+export default config;
---
+++ packages/ui/common/tailwind.config.ts
@@ -0,0 +1,9 @@
+import tailwindConfig from '../../tailwind-config/tailwind.config';
+
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  ...tailwindConfig,
+};
+
+export default config;
